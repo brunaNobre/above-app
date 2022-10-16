375665a0ed67c3ae25cc7d636fb9229a68e186a1
Bruna Nobre <bruna.nobre@bridge-mt.com>
2022-10-16 15:27:35 -0300

    chore(above): Install material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import './App.css';
 
+import { Button } from '@material-ui/core';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Above</p>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
       </header>
     </div>
   );
